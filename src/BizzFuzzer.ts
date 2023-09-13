2e67d72baa3b23f95b39347e22e421624ec1e0e3
HoKeiL <hokeileung1106@gmail.com>
2023-09-13 10:06:26 +0100

    BizzFuzzer task 2

---
+++ src/BizzFuzzer.ts
@@ -0,0 +1,50 @@
+class BizzFuzzer {
+    currentNum: number;
+    bizzWord: string;
+    fuzzWord: string;
+    bizzNum: number;
+    fuzzNum: number;
+
+    constructor(
+        bizzWord: string,
+        fuzzWord: string,
+        bizzNum: number,
+        fuzzNum: number,
+        currentNum = 1
+    ) {
+        this.currentNum = currentNum;
+        this.bizzWord = bizzWord;
+        this.fuzzWord = fuzzWord;
+        this.bizzNum = bizzNum;
+        this.fuzzNum = fuzzNum;
+    }
+
+    next() {
+        this.currentNum += 1;
+        if (
+            this.currentNum % this.bizzNum === 0 &&
+            this.currentNum % this.fuzzNum === 0
+        ) {
+            return `${this.bizzWord}${this.fuzzWord}`;
+        } else if (this.currentNum % this.fuzzNum === 0) {
+            return `${this.fuzzWord}`;
+        } else if (this.currentNum % this.bizzNum === 0) {
+            return `${this.bizzWord}`;
+        } else {
+            return this.currentNum;
+        }
+    }
+    reset() {
+        this.currentNum = 1;
+    }
+}
+
+const myBizzFuzzer = new BizzFuzzer("Boop", "Schnoop", 2, 3);
+
+console.log(myBizzFuzzer);
+console.log(myBizzFuzzer.next()); //"Boop"
+console.log(myBizzFuzzer.next()); //"Schnoop"
+console.log(myBizzFuzzer.next()); //"Boop"
+console.log(myBizzFuzzer.next()); //5
+myBizzFuzzer.reset();
+console.log(myBizzFuzzer.next()); //"Boop"
